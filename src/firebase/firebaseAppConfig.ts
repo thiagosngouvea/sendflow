ed6ca9164fc3ab08a7f18aa5db4d5c89cf5baa67
thiagosngouvea <thiagosngouvea@gmail.com>
2024-04-24 18:09:20 -0300

    fix: :bug: ajuste firebase e env

--- src/firebase/firebaseAppConfig.ts
+++ src/firebase/firebaseAppConfig.ts
@@ -7,13 +7,13 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBigpPO373iW3VhcztRA41iXUmI-7AZUbw",
-  authDomain: "sendflow-b1be7.firebaseapp.com",
-  databaseURL: "https://sendflow-b1be7-default-rtdb.firebaseio.com",
-  projectId: "sendflow-b1be7",
-  storageBucket: "sendflow-b1be7.appspot.com",
-  messagingSenderId: "934451855273",
-  appId: "1:934451855273:web:f03cb55f47f362fa03ceb8"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
